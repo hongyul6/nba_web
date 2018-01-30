972f00acd7a21f0d96bf304479317b700b1cca3b
hongyul6 <hongyul6@uci.edu>
2018-01-30 13:34:17 +0800

    stage 1

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,12 +5,21 @@ import {Profile} from './Profile';
 
 export class Main extends React.Component{
     state = {
-        playerId: nba.findPlayer('Stephen Curry').playerId,
+        playerId: nba.findPlayer('Kyle Singler').playerId,
+        playerInfo: {},
     }
+    componentDidMount(){
+        nba.stats.playerInfo({ PlayerID: this.state.playerId}).then((info) => {
+            const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+            console.log(playerInfo);
+            this.setState({playerInfo: playerInfo});
+        });
+    }
+
     render(){
         return (
             <div className="main">
-                <Profile playerId={this.state.playerId}/>
+                <Profile playerInfo={this.state.playerInfo}/>
                 <ShotChart playerId={this.state.playerId}/>
             </div>
         );
